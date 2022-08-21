f8636deb73a73e107f2421b81a7c74ddcd374757
Mathilde <mat.Grimal@gmail.com>
2022-08-21 20:07:10 +0200

    added sitemap page

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import * as fs from 'fs';
 import { loadDansesData } from '../lib/loadDansesData';
+import glob from 'glob';
+import { DansesProps } from '../types';
 
 const Sitemap = () => {
   return null;
@@ -8,33 +9,28 @@ const Sitemap = () => {
 
 export const getServerSideProps = async ({ res }: any) => {
   const BASE_URL = 'www.flamenco-zumba.com';
-
-  const staticPaths = fs
-    .readdirSync('pages')
-    .filter((staticPage) => {
-      return ![
-        'api',
-        'danses',
-        '_app.tsx',
-        '404.js',
-        'sitemap.xml.tsx',
-      ].includes(staticPage);
-    })
+  const pagesDir = 'pages/**/*.tsx';
+  let pagesPaths: Array<string> = glob.sync(pagesDir);
+  pagesPaths = pagesPaths
+    .filter((path: any) => !path.includes('['))
+    .filter((path: any) => !path.includes('/_'))
+    .filter((path: any) => !path.includes('404'))
     .map((staticPagePath) => {
       if (staticPagePath.split('.tsx')[0].includes('index')) {
         return `${BASE_URL}`;
       }
-      return `${BASE_URL}/${staticPagePath.split('.tsx')[0]}`;
+      return `${BASE_URL}/${
+        staticPagePath.split('pages/')[1].split('.tsx')[0]
+      }`;
     });
 
-  const danses = await loadDansesData();
+  const danses: DansesProps = await loadDansesData();
   console.log(danses);
   let dynamicPaths = [];
   for (const danse of danses.allDanses) {
     dynamicPaths.push(`${BASE_URL}/danses/${danse.slug}`);
   }
-  console.log(dynamicPaths);
-  const allPaths = [...staticPaths, ...dynamicPaths];
+  const allPaths = [...pagesPaths, ...dynamicPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
